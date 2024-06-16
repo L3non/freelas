612e34a54f6610376bea70b88a447bc21c2782d1
Bruno Fernandes <fernandesbruno01384@gmail.com>
2024-06-16 17:28:10 -0300

    MekaDisel loading screen

---
+++ meka_disel/scripts/script.js
@@ -0,0 +1,7 @@
+    /* == == Preloader == == */
+
+const preloader = document.querySelector('section#preloader');
+
+window.addEventListener('load', () => {
+    preloader.classList.add('remove');
+});
