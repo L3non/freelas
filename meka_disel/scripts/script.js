b8f0c6935ead400f7d0788379e1cc6cfbdced1ee
Bruno Fernandes <fernandesbruno01384@gmail.com>
2024-06-17 19:37:58 -0300

    header and hamburguer menu done

--- meka_disel/scripts/script.js
+++ meka_disel/scripts/script.js
@@ -5,3 +5,33 @@ const preloader = document.querySelector('section#preloader');
 window.addEventListener('load', () => {
     preloader.classList.add('remove');
 });
+
+    /* == == Add event on multiple elements */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+    /* == == Navbar toggler == == */
+
+const navbar = document.querySelector('nav#navbar');
+const navTogglers = document.getElementsByClassName('nav-toggler');
+const overlay = document.querySelector('div.overlay');
+
+const toggleNav = function () {
+    navbar.classList.toggle('active');
+    overlay.classList.toggle('active');
+    document.body.classList.toggle('nav-active');
+}
+
+addEventOnElements(navTogglers, 'click', toggleNav)
+
+    /* == == Header == == */
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+    header.classList[this.window.scrollY > 100 ? 'add' : 'remove']('active');
+})
